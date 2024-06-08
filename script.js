fb281ea8b0363d31efdd0e9d980f0208f0d92743
abdellahak <93621937+abdellahak@users.noreply.github.com>
2024-06-08 02:16:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -23,6 +23,9 @@ function getDeg(transformValue){
     return Number(result);
 }
 
+let date = new Date();
+secondes.style.transform = `translate(-50%, -100%) rotate(${date.getSeconds()*6}deg)`;
+
 function setTime(){
     let date = new Date();
     let hourTime = date.getHours()
@@ -34,16 +37,12 @@ function setTime(){
     let currentHourDeg = getDeg(hour.style.transform);
     let currentMinuteDeg = getDeg(minute.style.transform);
     let currentSecondDeg = getDeg(secondes.style.transform);
-    // let currentMinuteDeg = hour.style.transform.split('(')[2].replace('deg)','');
-    // let currentSecondesDeg = hour.style.transform.split('(')[2].replace('deg)','');
-
-
 
     hour.style.transform = `translate(-50%, -100%) rotate(${hourTime*15}deg)`;
     minute.style.transform = `translate(-50%, -100%) rotate(${minuteTime*6}deg)`;
     secondes.style.transform = `translate(-50%, -100%) rotate(${currentSecondDeg+6}deg)`;
 
-    time.innerText = `${hourMod12} : ${minuteTime} ${hourTime>12 ? "PM":"AM"}`;
+    time.innerText = `${hourMod12< 10? "0"+hourMod12:hourMod12} : ${minuteTime< 10? "0"+minuteTime:minuteTime} ${hourTime>12 ? "PM":"AM"}`;
     dateElement.innerText = `${days[date.getDate()-1]}, ${months[date.getMonth()]}, ${date.getDate()}`;
 }
 setTime()
